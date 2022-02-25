f8e0f183e74b821521ccb2b875e5404ef114b191
Gugustinette <mercier.augustin@outlook.fr>
2022-02-25 08:31:58 +0100

    Init Client

---
+++ client/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="app">
+    <Navbar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+// Components
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style lang="scss">
+:root {
+  --primary: #42b983;
+  --secondary: #2c3e50;
+
+  --background: #f5f5f5;
+  --surface: rgb(206, 206, 206);
+
+  --font: #333;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: var(--font);
+}
+
+a {
+  color: var(--primary);
+}
+</style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <nav>
+    <div class="logo">
+      <router-link to="/">
+        <img src="https://via.placeholder.com/40" alt="Logo" />
+      </router-link>
+    </div>
+    <div class="links">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+nav {
+  width: 100%;
+  height: 40px;
+  background: var(--surface);
+  display: flex;
+  flex-direction: row;
+
+  .logo {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+
+  .links {
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+
+    a {
+      height: 100%;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      text-decoration: none;
+      color: var(--font);
+      font-size: 1.2rem;
+      font-weight: bold;
+      padding: 0 1rem;
+      transition: all 0.2s ease-in-out;
+
+      &:hover {
+        background: var(--primary);
+        color: var(--background);
+      }
+
+      &.router-link-active {
+        background: var(--primary);
+        color: var(--background);
+      }
+    }
+  }
+}
+</style>
