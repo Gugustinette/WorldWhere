0c6dc394ec4eeb99cf748c8aa0afa432e76fd806
Gugustinette <mercier.augustin@outlook.fr>
2022-02-28 17:39:10 +0100

    Fix Country Request

--- server/controllers/country.js
+++ server/controllers/country.js
@@ -12,7 +12,9 @@ const CountryData = require('../models/CountryData');
 
 /**
  * Get Countries - Get countries from database
- * @param {*} req.query.data date of the data
+ * @param {*} req.query.dateStart date of the data
+ * @param {*} req.query.dateEnd date of the data
+ * @param {*} req.query.full if true, return all countries, else return only the countries with data
  */
 exports.getCountries = (req, res, next) => {
     // Get recent data from database
@@ -31,12 +33,14 @@ exports.getCountries = (req, res, next) => {
                 countries.forEach(country => {
                     // Find corresponding country data
                     var countryData = countriesData.find(countryData => countryData.country.toString() === country._id.toString());
-                    countriesWithData.push({
-                        country: {
-                            name: country.name
-                        },
-                        percentageOfPopularity: countryData ? countryData.percentageOfPopularity : undefined
-                    });
+                    if (countryData || req.query.full) {
+                        countriesWithData.push({
+                            country: {
+                                name: country.name
+                            },
+                            percentageOfPopularity: countryData ? countryData.percentageOfPopularity : undefined
+                        });
+                    }
                 });
 
                 res.status(200).json({
