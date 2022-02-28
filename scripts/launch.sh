404941eeb038716d7df4ae78563c7e492259a756
Gugustinette <mercier.augustin@outlook.fr>
2022-02-28 21:28:43 +0100

    Launch Client

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,4 +1,5 @@
 # Launch the whole project
 # Script should be run from root folder
 
-pm2 start npm --name "WorldWhere-server" -- run "server"
+pm2 start npm --name "WorldWhere-Server" -- run "server"
+pm2 start npm --name "WorldWhere-Client" -- run "client"
