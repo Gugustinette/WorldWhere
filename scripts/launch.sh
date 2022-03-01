1b63ab929cf65444ffc01ff8760caa7d91acf2fd
Gugustinette <mercier.augustin@outlook.fr>
2022-03-01 22:42:08 +0100

    Fix Name Process Schedule

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -6,4 +6,4 @@ pm2 start npm --name "WW-Server" -- run "server"
 pm2 start npm --name "WW-Client" -- run "build:client"
 
 # Schedule - Data Updater
-pm2 start npm --name "WW-Client" -- run "schedule"
+pm2 start npm --name "WW-Schedule" -- run "schedule"
