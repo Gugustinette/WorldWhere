87deb94368331a85d42391be70e37e77c83d85a4
Gugustinette <mercier.augustin@outlook.fr>
2022-03-01 13:08:47 +0100

    Schedule Offset

--- index.js
+++ index.js
@@ -3,17 +3,19 @@
 */
 
 
+const TimeBetweenUpdate = 30; // seconds
+const OffsetBeforeStart = TimeBetweenUpdate / 2;
+
+
 // Import bree for schedule management
 const Bree = require('bree');
 
-const bree = new Bree({
+const breeCountries = new Bree({
     jobs: [
         // 'fetchCountriesData',
-        // 'fetchRegionsData',
 
-        // Run fetchCountriesData and fetchRegionsData job every minute
-        { name: 'fetchCountriesData', interval: '30s' },
-        { name: 'fetchRegionsData', interval: '30s' },
+        // Run fetchCountriesData and fetchRegionsData job every 30 seconds
+        { name: 'fetchCountriesData', interval: `${TimeBetweenUpdate}s` },
 
         /*
         // Run clean logs job At 02:17 AM on Sunday (with cron schedule)
@@ -22,4 +24,20 @@ const bree = new Bree({
     ]
 });
 
-bree.start();
+const breeRegions = new Bree({
+    jobs: [
+        // 'fetchRegionsData',
+
+        // Run fetchRegionsData job every 30 seconds
+        { name: 'fetchRegionsData', interval: `${TimeBetweenUpdate}s` },
+    ]
+});
+
+
+
+breeCountries.start();
+
+// breeRegions.start() offset
+setTimeout(() => {
+    breeRegions.start();
+}, OffsetBeforeStart * 1000);
